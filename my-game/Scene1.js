2569b70123e2073acf3a14342e390e5025c46db3
James D.John <duonghht1419@gmail.com>
2020-06-07 10:13:49 +0700

    Thêm ảnh kiếm được từ mạng và dựng thành công màn hình chính thứ nhất #1

--- my-game/Scene1.js
+++ my-game/Scene1.js
@@ -7,11 +7,55 @@ class Scene1 extends Phaser.Scene{
 
     //load image and audio;
     preload() {
+        this.load.image('backGround', 'Assets/backGround.png')
+        this.load.image('frameWork', 'Assets/frameWork.png')
 
+        this.load.image('1', 'Assets/1.png')
+        this.load.image('2', 'Assets/2.png')
+
+        this.load.image('StartSheet', 'Assets/StartSheet.png')
+        this.load.image('startButton1', 'Assets/startButton1.png')
+        this.load.image('startButton2', 'Assets/startButton2.png')
     }
 
     //create game;
     create(){
+        this.backGround = this.add.image(0, 0, 'backGround').setOrigin(0, 0)
+        this.frameWork = this.add.image(325, 20, 'frameWork').setOrigin(0, 0)
+
+        this.scene1()
+    }
+
+    startButton(){
+        this.StartSheet = this.add.image(329, 20, 'StartSheet').setOrigin(0, 0)
+
+        this.startButton1 = this.add.image(660, 200, 'startButton1').setOrigin(0, 0)
+        this.startButton1.setInteractive().on('pointerover', () => {
+            this.startButton1.destroy()
+
+            this.startButton2 = this.add.image(625, 175, 'startButton2').setOrigin(0, 0)
+            this.startButton2.setInteractive().on('pointerup', () => {
+                this.StartSheet.destroy()
+                this.startButton2.destroy()
+                this.scene2()
+            })
+            this.startButton2.on('pointerout', () =>{
+                this.StartSheet.destroy()
+                this.startButton2.destroy()
+                this.startButton1.destroy()
+                this.startButton();
+            })
+        })
+
+    }
+
+    scene1(){
+        this.carChild = this.add.image(370, 450, '1').setOrigin(0, 0)
+        this.carFather = this.add.image(1000, 350, '2').setOrigin(0, 0)
+        this.startButton()
+    }
+
+    scene2(){
 
     }
 
--- my-game/game.js
+++ my-game/game.js
@@ -1,12 +1,10 @@
-
-
 const config = {
 	type: Phaser.AUTO,
 	width: 1525,
 	height: 770,
 	parent: 'phaser-game',
 	background: 0x000000,
-	scene: [Scene1,Scene2,Scene3]
+	scene: [Scene1, Scene2]
 };
 
 let game = new Phaser.Game(config);
