e89e8a1cab4d09490677f980778be34802499646
James D.John <duonghht1419@gmail.com>
2020-06-09 12:41:57 +0700

    Thêm thành công kéo thả đối tượng cho game

--- my-game/Scene1.js
+++ my-game/Scene1.js
@@ -11,8 +11,8 @@ class Scene1 extends Phaser.Scene{
         this.load.image('backGround', 'Assets/backGround.png')
         this.load.image('frameWork', 'Assets/frameWork.png')
 
-        this.load.image('1', 'Assets/1.png')
-        this.load.image('2', 'Assets/2.png')
+        this.load.image('carChild', 'Assets/carChild.png')
+        this.load.image('carFather', 'Assets/carFather.png')
 
         this.load.image('StartSheet', 'Assets/StartSheet.png')
         this.load.image('startButton1', 'Assets/startButton1.png')
@@ -21,6 +21,38 @@ class Scene1 extends Phaser.Scene{
         this.load.image('bridge', 'Assets/bridge.png')
         this.load.image('bridge1', 'Assets/bridge1.png')
         this.load.image('bridge2', 'Assets/bridge2.png')
+
+        this.load.image('0', 'Assets/0.png')
+        this.load.image('1', 'Assets/1.png')
+        this.load.image('2', 'Assets/2.png')
+        this.load.image('3', 'Assets/3.png')
+        this.load.image('4', 'Assets/4.png')
+        this.load.image('5', 'Assets/5.png')
+        this.load.image('6', 'Assets/6.png')
+        this.load.image('7', 'Assets/7.png')
+        this.load.image('8', 'Assets/8.png')
+        this.load.image('9', 'Assets/9.png')
+        this.load.image('10', 'Assets/10.png')
+        this.load.image('11', 'Assets/11.png')
+        this.load.image('12', 'Assets/12.png')
+        this.load.image('13', 'Assets/13.png')
+        this.load.image('14', 'Assets/14.png')
+        this.load.image('15', 'Assets/15.png')
+        this.load.image('16', 'Assets/16.png')
+        this.load.image('17', 'Assets/17.png')
+        this.load.image('18', 'Assets/18.png')
+        this.load.image('19', 'Assets/19.png')
+        this.load.image('20', 'Assets/20.png')
+        this.load.image('21', 'Assets/21.png')
+        this.load.image('22', 'Assets/22.png')
+        this.load.image('23', 'Assets/23.png')
+        this.load.image('24', 'Assets/24.png')
+        this.load.image('25', 'Assets/25.png')
+        this.load.image('26', 'Assets/26.png')
+        this.load.image('27', 'Assets/27.png')
+        this.load.image('28', 'Assets/28.png')
+        this.load.image('29', 'Assets/29.png')
+        this.load.image('30', 'Assets/30.png')
     }
 
     //create game;
@@ -55,8 +87,8 @@ class Scene1 extends Phaser.Scene{
     }
 
     scene1(){
-        this.carChild = this.add.image(370, 450, '1').setOrigin(0, 0)
-        this.carFather = this.add.image(1020, 350, '2').setOrigin(0, 0)
+        this.carChild = this.add.image(370, 450, 'carChild').setOrigin(0, 0)
+        this.carFather = this.add.image(1020, 350, 'carFather').setOrigin(0, 0)
         this.startButton()
     }
 
@@ -104,19 +136,69 @@ class Scene1 extends Phaser.Scene{
             if(data4 !== data1 && data4 !== data2 && data4 !== data3) break
         }
 
-        this.setLocalDataNumber(data1, arrNumber.indexOf(data1))
-        this.setLocalDataNumber(data2, arrNumber.indexOf(data2))
-        this.setLocalDataNumber(data3, arrNumber.indexOf(data3))
-        this.setLocalDataNumber(data4, arrNumber.indexOf(data4))
+        this.setLocalDataNumberConst(data1, arrNumber.indexOf(data1))
+        this.setLocalDataNumberConst(data2, arrNumber.indexOf(data2))
+        this.setLocalDataNumberConst(data3, arrNumber.indexOf(data3))
+        this.setLocalDataNumberConst(data4, arrNumber.indexOf(data4))
 
+        let pos = 0
         for(let i = 0; i < 9; i++){
-            console.log(arrNumber[i])
+            if(i === arrNumber.indexOf(data1) || i === arrNumber.indexOf(data2) ||
+                i === arrNumber.indexOf(data3) || i === arrNumber.indexOf(data4)){
+            }
+            else{
+                this.setLocalZones(i)
+                this.setLocalDataNumberDynamic(arrNumber[i], pos++, i)
+            }
         }
 
         //return arrNumber
     }
 
-    setLocalDataNumber(dataNumber, local){
+    setLocalZones(local){
+
+        if(local == 0){
+            this.bridge1 = this.add.image(480, 430, 'bridge1').setOrigin(0, 0)
+        }
+        else if(local == 1){
+            this.bridge1 = this.add.image(540, 430, 'bridge1').setOrigin(0, 0)
+        }
+        else if(local == 2){
+            this.bridge1 = this.add.image(600, 430, 'bridge1').setOrigin(0, 0)
+        }
+        else if(local == 3){
+            this.bridge1 = this.add.image(660, 430, 'bridge1').setOrigin(0, 0)
+        }
+        else if(local == 4){
+            this.bridge1 = this.add.image(720, 430, 'bridge1').setOrigin(0, 0)
+        }
+        else if(local == 5){
+            this.bridge1 = this.add.image(780, 430, 'bridge1').setOrigin(0, 0)
+        }
+        else if(local == 6){
+            this.bridge1 = this.add.image(840, 430, 'bridge1').setOrigin(0, 0)
+        }
+        else if(local == 7){
+            this.bridge1 = this.add.image(900, 430, 'bridge1').setOrigin(0, 0)
+        }
+        else{
+            this.bridge1 = this.add.image(960, 430, 'bridge1').setOrigin(0, 0)
+        }
+
+        this.bridge1.setInteractive()
+
+        this.input.on('gameobjectover', function (pointer, gameObject) {
+            gameObject.setTint(0xFFC400)
+        })
+
+        this.input.on('gameobjectout', function (pointer, gameObject) {
+            gameObject.clearTint()
+        })
+
+    }
+
+    setLocalDataNumberConst(dataNumber, local){
+
         if(local == 0){
             this.dataGroup(dataNumber, 480, 430)
         }
@@ -144,21 +226,93 @@ class Scene1 extends Phaser.Scene{
         else{
             this.dataGroup(dataNumber, 960, 430)
         }
+
+    }
+
+    setLocalDataNumberDynamic(dataNumber, local, checkOrder){
+        if(local == 2){
+            this.dataGroup1(dataNumber, 550, 525, checkOrder)
+        }
+        else if(local == 0){
+            this.dataGroup1(dataNumber, 650, 525, checkOrder)
+        }
+        else if(local == 4){
+            this.dataGroup1(dataNumber, 750, 525, checkOrder)
+        }
+        else if(local == 1){
+            this.dataGroup1(dataNumber, 850, 525, checkOrder)
+        }
+        else{
+            this.dataGroup1(dataNumber, 950, 525, checkOrder)
+        }
     }
 
     dataGroup(dataNumber, x, y){
         // 960 430
         this.bridge = this.add.image(x, y, 'bridge').setOrigin(0, 0)
-        this.dataNumber = this.add.text(x + 5, y + 30, dataNumber, {font: '35px Arial', fill: 'red'}).setOrigin(0, 0)
+        this.dataNumber = this.add.image(x + 5, y + 30, dataNumber).setOrigin(0, 0)
+
+    }
+
+    dataGroup1(dataNumber, x, y, checkOrder){
+
+        var dataNumber = this.add.image(x + 5, y + 30, dataNumber).setOrigin(0, 0).setInteractive()
+
+        this.input.setDraggable(dataNumber);
+
+        this.input.on('dragstart', function (pointer, gameObject) {
+
+            this.children.bringToTop(gameObject);
+
+        }, this);
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+
+        });
+
+        this.input.on('dragenter', function (pointer, gameObject, dropZone) {
+
+
+        });
+
+        this.input.on('dragleave', function (pointer, gameObject, dropZone) {
+
+
+        });
+
+        this.input.on('drop', function (pointer, gameObject, dropZone) {
+
+        });
+
+        this.input.on('dragend', function (pointer, gameObject, dropped) {
+
+            if (!dropped)
+            {
+                gameObject.x = gameObject.input.dragStartX;
+                gameObject.y = gameObject.input.dragStartY;
+            }
+
+        });
     }
 
     scene2(){
         this.randomDataNumber()
     }
 
+    animationCar(){
+        this.children.bringToTop(this.carChild)
+        if(this.carChild.x < 1000) {
+            this.carChild.x += 1.5
+        }
+    }
+
     //Update;
     update(){
 
+        this.animationCar()
     }
 
 }
