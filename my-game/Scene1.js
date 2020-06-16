03cec08f9ca4177b87e84ee6877fff3e902e4dc2
James D.John <duonghht1419@gmail.com>
2020-06-17 00:27:35 +0700

    Hoàn thiện chức năng kéo thả #2

--- my-game/Scene1.js
+++ my-game/Scene1.js
@@ -1,4 +1,8 @@
-let arrNumber = [];
+let arrNumber = []
+let label
+let numberPlay = 5
+let zone = []
+let data = []
 
 class Scene1 extends Phaser.Scene{
 
@@ -59,6 +63,8 @@ class Scene1 extends Phaser.Scene{
     create(){
         this.backGround = this.add.image(0, 0, 'backGround').setOrigin(0, 0)
         this.frameWork = this.add.image(325, 20, 'frameWork').setOrigin(0, 0)
+        label = this.add.text(0, 0, '', { font: "48px Arial Black", fill: "#c51b7d" });
+
 
         this.scene1()
     }
@@ -136,23 +142,156 @@ class Scene1 extends Phaser.Scene{
             if(data4 !== data1 && data4 !== data2 && data4 !== data3) break
         }
 
-        this.setLocalDataNumberConst(data1, arrNumber.indexOf(data1))
-        this.setLocalDataNumberConst(data2, arrNumber.indexOf(data2))
-        this.setLocalDataNumberConst(data3, arrNumber.indexOf(data3))
-        this.setLocalDataNumberConst(data4, arrNumber.indexOf(data4))
-
         let pos = 0
+
         for(let i = 0; i < 9; i++){
-            if(i === arrNumber.indexOf(data1) || i === arrNumber.indexOf(data2) ||
-                i === arrNumber.indexOf(data3) || i === arrNumber.indexOf(data4)){
+
+            if(i === arrNumber.indexOf(data1)){
+                data[i] = this.setLocalDataNumberConst(data1, i)
+            }
+            else if(i === arrNumber.indexOf(data2)){
+                data[i] = this.setLocalDataNumberConst(data2, i)
+            }
+            else if(i === arrNumber.indexOf(data3)){
+                data[i] = this.setLocalDataNumberConst(data3, i)
+            }
+            else if(i === arrNumber.indexOf(data4)){
+                data[i] = this.setLocalDataNumberConst(data4, i)
             }
             else{
-                this.setLocalZones(i)
-                this.setLocalDataNumberDynamic(arrNumber[i], pos++, i)
+
+                zone[i] = this.setLocalZones(i).setInteractive()
+
+                data[i] = this.setLocalDataNumberDynamic(arrNumber[i], pos++, i)
             }
         }
 
-        //return arrNumber
+        this.input.on('dragstart', function (pointer, gameObject) {
+
+            this.children.bringToTop(gameObject);
+
+        }, this);
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+
+        });
+
+        this.input.on('gameobjectdown', function (pointer, gameObject) {
+
+            if(gameObject.name === 0){
+                zone[0].input.dropZone = true;
+                //console.log("0 mo")
+            }
+            else if(gameObject.name === 1){
+                zone[1].input.dropZone = true;
+                //console.log("1 mo")
+            }
+            else if(gameObject.name === 2){
+                zone[2].input.dropZone = true;
+                //console.log("2 mo")
+            }
+            else if(gameObject.name === 3){
+                zone[3].input.dropZone = true;
+                //console.log("3 mo")
+            }
+            else if(gameObject.name === 4){
+                zone[4].input.dropZone = true;
+                //console.log("4 mo")
+            }
+            else if(gameObject.name === 5){
+                zone[5].input.dropZone = true;
+                //console.log("5 mo")
+            }
+            else if(gameObject.name === 6){
+                zone[6].input.dropZone = true;
+                //console.log("6 mo")
+            }
+            else if(gameObject.name === 7){
+                zone[7].input.dropZone = true;
+                //console.log("7 mo")
+            }
+            else {
+                zone[8].input.dropZone = true;
+                //console.log("8 mo")
+            }
+
+        });
+
+        this.input.on('gameobjectup', function (pointer, gameObject) {
+
+            if(gameObject.name === 0){
+                zone[0].input.dropZone = false;
+                //console.log("0 dong")
+            }
+            else if(gameObject.name === 1){
+                zone[1].input.dropZone = false;
+                //console.log("1 dong")
+            }
+            else if(gameObject.name === 2){
+                zone[2].input.dropZone = false;
+                //console.log("2 dong")
+            }
+            else if(gameObject.name === 3){
+                zone[3].input.dropZone = false;
+                //console.log("3 dong")
+            }
+            else if(gameObject.name === 4){
+                zone[4].input.dropZone = false;
+                //console.log("4 dong")
+            }
+            else if(gameObject.name === 5){
+                zone[5].input.dropZone = false;
+                //console.log("5 dong")
+            }
+            else if(gameObject.name === 6){
+                zone[6].input.dropZone = false
+                //console.log("6 dong")
+            }
+            else if(gameObject.name === 7){
+                zone[7].input.dropZone = false
+                //console.log("7 dong")
+            }
+            else {
+                zone[8].input.dropZone = false
+                //console.log("8 dong")
+            }
+
+        });
+
+        this.input.on('dragenter', function (pointer, gameObject, dropZone) {
+
+
+        });
+
+        this.input.on('dragleave', function (pointer, gameObject, dropZone) {
+
+
+        });
+
+        this.input.on('drop', function (pointer, gameObject, dropZone) {
+
+            gameObject.x = dropZone.x + 5
+            gameObject.y = dropZone.y + 30
+
+            numberPlay--
+
+            gameObject.input.enabled = false;
+
+        });
+
+        this.input.on('dragend', function (pointer, gameObject, dropped) {
+
+            if (!dropped)
+            {
+                gameObject.x = gameObject.input.dragStartX;
+                gameObject.y = gameObject.input.dragStartY;
+            }
+
+        });
+
     }
 
     setLocalZones(local){
@@ -185,117 +324,78 @@ class Scene1 extends Phaser.Scene{
             this.bridge1 = this.add.image(960, 430, 'bridge1').setOrigin(0, 0)
         }
 
-        this.bridge1.setInteractive()
-
-        this.input.on('gameobjectover', function (pointer, gameObject) {
-            gameObject.setTint(0xFFC400)
-        })
-
-        this.input.on('gameobjectout', function (pointer, gameObject) {
-            gameObject.clearTint()
-        })
+        return this.bridge1
 
     }
 
     setLocalDataNumberConst(dataNumber, local){
 
-        if(local == 0){
-            this.dataGroup(dataNumber, 480, 430)
-        }
-        else if(local == 1){
-            this.dataGroup(dataNumber, 540, 430)
-        }
-        else if(local == 2){
-            this.dataGroup(dataNumber, 600, 430)
-        }
-        else if(local == 3){
-            this.dataGroup(dataNumber, 660, 430)
-        }
-        else if(local == 4){
-            this.dataGroup(dataNumber, 720, 430)
-        }
-        else if(local == 5){
-            this.dataGroup(dataNumber, 780, 430)
-        }
-        else if(local == 6){
-            this.dataGroup(dataNumber, 840, 430)
-        }
-        else if(local == 7){
-            this.dataGroup(dataNumber, 900, 430)
-        }
-        else{
-            this.dataGroup(dataNumber, 960, 430)
+        switch (local) {
+            case 0:
+                this.bridge = this.add.image(480, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(485, 460, dataNumber).setOrigin(0, 0)
+                break
+            case 1:
+                this.bridge = this.add.image(540, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(545, 460, dataNumber).setOrigin(0, 0)
+                break
+            case 2:
+                this.bridge = this.add.image(600, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(605, 460, dataNumber).setOrigin(0, 0)
+                break
+            case 3:
+                this.bridge = this.add.image(660, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(665, 460, dataNumber).setOrigin(0, 0)
+                break
+            case 4:
+                this.bridge = this.add.image(720, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(725, 460, dataNumber).setOrigin(0, 0)
+                break
+            case 5:
+                this.bridge = this.add.image(780, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(785, 460, dataNumber).setOrigin(0, 0)
+                break
+            case 6:
+                this.bridge = this.add.image(840, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(845, 460, dataNumber).setOrigin(0, 0)
+                break
+            case 7:
+                this.bridge = this.add.image(900, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(905, 460, dataNumber).setOrigin(0, 0)
+                break
+            case 8:
+                this.bridge = this.add.image(960, 430, 'bridge').setOrigin(0, 0)
+                this.data = this.add.image(965, 460, dataNumber).setOrigin(0, 0)
+                break
         }
 
+        return this.data
     }
 
     setLocalDataNumberDynamic(dataNumber, local, checkOrder){
-        if(local == 2){
-            this.dataGroup1(dataNumber, 550, 525, checkOrder)
+
+        switch (local) {
+            case 0:
+                this.data = this.add.image(655, 555, dataNumber).setName(checkOrder)
+                break
+            case 1:
+                this.data = this.add.image(855, 555, dataNumber).setName(checkOrder)
+                break
+            case 2:
+                this.data = this.add.image(555, 555, dataNumber).setName(checkOrder)
+                break
+            case 3:
+                this.data = this.add.image(955, 555, dataNumber).setName(checkOrder)
+                break
+            case 4:
+                this.data = this.add.image(755, 555, dataNumber).setName(checkOrder)
+                break
         }
-        else if(local == 0){
-            this.dataGroup1(dataNumber, 650, 525, checkOrder)
-        }
-        else if(local == 4){
-            this.dataGroup1(dataNumber, 750, 525, checkOrder)
-        }
-        else if(local == 1){
-            this.dataGroup1(dataNumber, 850, 525, checkOrder)
-        }
-        else{
-            this.dataGroup1(dataNumber, 950, 525, checkOrder)
-        }
-    }
 
-    dataGroup(dataNumber, x, y){
-        // 960 430
-        this.bridge = this.add.image(x, y, 'bridge').setOrigin(0, 0)
-        this.dataNumber = this.add.image(x + 5, y + 30, dataNumber).setOrigin(0, 0)
+        this.data.setOrigin(0, 0).setInteractive()
+        this.input.setDraggable(this.data);
 
-    }
-
-    dataGroup1(dataNumber, x, y, checkOrder){
-
-        var dataNumber = this.add.image(x + 5, y + 30, dataNumber).setOrigin(0, 0).setInteractive()
-
-        this.input.setDraggable(dataNumber);
-
-        this.input.on('dragstart', function (pointer, gameObject) {
-
-            this.children.bringToTop(gameObject);
-
-        }, this);
-
-        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-
-            gameObject.x = dragX;
-            gameObject.y = dragY;
-
-        });
-
-        this.input.on('dragenter', function (pointer, gameObject, dropZone) {
-
-
-        });
-
-        this.input.on('dragleave', function (pointer, gameObject, dropZone) {
-
-
-        });
-
-        this.input.on('drop', function (pointer, gameObject, dropZone) {
-
-        });
-
-        this.input.on('dragend', function (pointer, gameObject, dropped) {
-
-            if (!dropped)
-            {
-                gameObject.x = gameObject.input.dragStartX;
-                gameObject.y = gameObject.input.dragStartY;
-            }
-
-        });
+        return this.data
     }
 
     scene2(){
@@ -309,10 +409,30 @@ class Scene1 extends Phaser.Scene{
         }
     }
 
+    destroyObject(){
+        for(var i = 0; i < 9; i++){
+            data[i].destroy()
+        }
+    }
+
     //Update;
     update(){
 
-        this.animationCar()
+        if(numberPlay === 0) {
+
+            this.time.delayedCall(2000, function Correct() {
+
+                this.destroyObject()
+
+                this.time.delayedCall(2000, function Correct() {
+
+                    this.animationCar()
+
+                }, [], this);
+
+            }, [], this);
+        }
+
     }
 
 }
